2919f312377b479dd934e90ebace287cacefbedf
Kevin McAnoy <kmcanoy@redhat.com>
2017-03-07 15:42:46 -0800

    Update via PUT instead of properties file

--- pod/get_pods.sh
+++ pod/get_pods.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/ksh
 #
 set -x
-[ $# -eq 0 ] && { echo "Usage: $0 -d <dc> -n <project> -f <properties file>"; exit 1; }
+[ $# -eq 0 ] && { echo "Usage: $0 -d <dc> -n <project>" ; exit 1; }
 
 while [[ $# -gt 1 ]]
 do
@@ -10,10 +10,6 @@ key="$1"
 case $key in
     -n)
     PROJECT="$2"
-    shift # past argument
-    ;;
-    -f)
-    PROP_FILE="$2"
     shift # past argument
     ;;
     -d)
@@ -32,14 +28,14 @@ IPS=""
 # Gets a list of list of pods in the project namespace (ex. apollo) filtering for application (ex. searcher)
 # and creates a comma separated list of application URLs 
 # TODO this port should be parameterized
-oc get pods -o jsonpath='{range .items[*]}{.metadata.name} {.status.podIP} {.status.phase}
+oc get pods -l app=${DC} -o jsonpath='{range .items[*]}{.metadata.name} {.status.podIP} {.status.phase}
 {end}' -n ${PROJECT} | egrep "^${DC}-.*-..... .*Running$" | \
 while read POD IP PHASE ; do
-   IPS="$IPS,http:\/\/$IP:8080"
+   IPS="$IPS,\"$IP\""
 done
 
 IPS=${IPS#?}
 
 echo $IPS
 
-sed -i -e "s/^\(apollo\.slave\.urls\s*=\s*\).*\$/\1$IPS/" $PROP_FILE
+curl -H "Content-Type: application/json" -X PUT -d "[$IPS]" $SPRING_APP_URL
